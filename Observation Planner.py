2122219eefe46844d36385edfa23b281103e0391
Paul <fork@quietfusion.com>
2024-01-11 09:33:21 -0500

    Code as used to produce 2024-01-10 submission

--- Observation Planner.py
+++ Observation Planner.py
@@ -11,6 +11,8 @@ import pandas as pd
 def pick_targets():
     conn = connect("../django/QuadEB/db.sqlite3")
 
+    targets = pd.read_sql("select * from tom_target;", conn, index_col="id")
+
     observing_lists =[
         "LBT Spectra from pepsi_files_database.txt|Robby|2023-11-22",
         "Featured targets from 2023-11-27 meeting",
@@ -30,9 +32,9 @@ def pick_targets():
             conn)["local_id"])
         list_members[observing_list] = list_targets
 
-    # desired_targets = functools.reduce(set.union, list_members.values())
-    desired_targets = list_members["Featured targets from 2023-11-27 meeting"].union(list_members["High Quality Non Detections from Speckle-High-Quality-Non-Detections-Table|Jimmy|2023-11-22"])
-    desired_targets = desired_targets.intersection(list_members["brighter than 13"])
+    # desired_targets = list_members["Featured targets from 2023-11-27 meeting"].union(list_members["High Quality Non Detections from Speckle-High-Quality-Non-Detections-Table|Jimmy|2023-11-22"])
+    desired_targets = list_members["Resolved Binaries from Speckle-Resolved-Table|Jimmy|2023-11-22"].intersection(list_members["brighter than 13"])
+    print("Num candidates:",len(desired_targets))
 
     target_table = Table.from_pandas(pd.read_sql(f"select * from tom_target where local_id in ('{"', '".join(desired_targets)}');", conn))
 
@@ -48,14 +50,15 @@ def pick_targets():
             ),
             name=local_id,
         )
-        for local_id, ra, dec, pmra, pmdec in target_table[["local_id", "ra", "dec", "pmra", "pmdec"]]
+        for local_id, ra, dec, pmra, pmdec, source in target_table[["local_id", "ra", "dec", "pmra", "pmdec", "source"]]
+        if source == "Kostov 2022 arXiv:2202.05790"
     ]
 
     observer = ap.Observer.at_site("lbt")
 
     constraints = [
         ap.AltitudeConstraint(10*u.deg, 80*u.deg),
-        ap.AirmassConstraint(1.6),
+        ap.AirmassConstraint(2),
         # ap.AtNightConstraint.twilight_civil(),
         ]
 
@@ -68,28 +71,21 @@ def pick_targets():
         "2023-12-12",
         ]
 
-    observable_targets, earlies = {}, {}
+    observable_targets = set()
     for observing_night_utc in observing_nights_utc:
         #TODO: fix shitty handling of time
-        early_in_session = Time([f"{observing_night_utc} 00:00", f"{observing_night_utc} 04:00"])
-        early_observable = ap.is_observable(constraints, observer, targets, early_in_session)
-        rest_of_session = Time([f"{observing_night_utc} 04:00", f"{observing_night_utc} 12:00"])
-        later_observable = ap.is_observable(constraints, observer, targets, rest_of_session)
-        early_ids = set(target_table[early_observable]["local_id"])
-        later_ids = set(target_table[later_observable]["local_id"])
-        early_setters = early_ids.difference(later_ids)
-        earlies[observing_night_utc] = early_setters
-        observable_targets[observing_night_utc] = early_ids | later_ids
+        observing_session = Time([f"{observing_night_utc} 00:00", f"{observing_night_utc} 04:00"])
+        observable_tonight = ap.is_observable(constraints, observer, targets, observing_session)
+        for observable, target in zip(observable_tonight, targets):
+            if observable:
+                observable_targets.add(target.name)
         # print(f"{observing_night_utc} {len(early_setters):2d}/{len(later_ids):2d} {", ".join(early_setters)}")
         # print(observing_night_utc)
         # for id in sorted(early_ids | later_ids):
         #     print(id)
-    import functools
-    common_earlies = functools.reduce(set.intersection, earlies.values())
-    # print("earlies", "', '".join(common_earlies))
-    common_targets = functools.reduce(set.intersection, observable_targets.values())
-    # print("lates", "', '".join(common_targets))
-    print("', '".join(common_earlies | common_targets))
+    print(len(observable_targets), "targets observable")
+    print("\n".join(observable_targets))
+
 
 
 if __name__ == "__main__":
